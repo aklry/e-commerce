33da6428acdb0fcbbccc09168bb62f56022c5df5
Aklry <74409739+aklry@users.noreply.github.com>
2023-06-25 23:04:11 +0800

    移动端自适应初始化

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -11,3 +11,12 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+//设置html字体大小,即设置单位rem的大小
+window.onload = function () {
+  document.documentElement.style.fontSize = (document.documentElement.clientWidth / 750) * 100 + 'px'
+}
+
+window.onresize = function () {
+  document.documentElement.style.fontSize = (document.documentElement.clientWidth / 750) * 100 + 'px'
+}
