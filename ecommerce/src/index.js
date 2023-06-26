cae7dfed86018915484ed81e995f0b3e0d4df4ff
Aklry <74409739+aklry@users.noreply.github.com>
2023-06-26 13:12:30 +0800

    更改组件变量的值

--- ecommerce/src/index.js
+++ ecommerce/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import './global.css'
 
 const store = createStore(() => { }, composeWithDevTools())
 ReactDOM.render(
