eb6310f3de120b4052929b94212f1def21990a8a
Aklry <74409739+aklry@users.noreply.github.com>
2023-06-29 12:56:24 +0800

    配置分页页面的路由

--- ecommerce/src/router/index.js
+++ ecommerce/src/router/index.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
+import Types from '../pages/Types'
 
 
 export default (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={ <Home /> } />
+            <Route path='/types' element={ <Types /> } />
         </Routes>
     </BrowserRouter>
 )
