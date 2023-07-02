cbeb895c1dbfd46dda16ed67ba2fc21f6a0636d4
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-02 12:12:25 +0800

    将调用后端相关收藏业务接口封装成方法

--- ecommerce/src/api/product/index.js
+++ ecommerce/src/api/product/index.js
@@ -8,7 +8,10 @@ const base = {
     getProductTypeId: '/types/getTypeId',
     getBrandByTypeId: '/product/getBrand',
     getProduct: '/product/getProduct',
-    getDetails: '/product/getDetails'
+    getDetails: '/product/getDetails',
+    starProduct: '/product/star',
+    cancelStarProduct: '/product/cancelStar',
+    getListStared: '/product/listStared'
 }
 const api = {
     getList:  pid =>  global.axios.get(baseUrl + base.getTypeList, {params: {pid}}), //根据商品pid获取数据
@@ -16,6 +19,9 @@ const api = {
     getProductTypeId: name => global.axios.get(baseUrl + base.getProductTypeId, { params: { name } }),//根据商品搜索的关键词获取商品类型的id
     getBrandByTypeId: id => global.axios.get(baseUrl + base.getBrandByTypeId, { params: { productType: id } }), //根据商品类型id获取品牌数据
     getProduct: params => global.axios.get(baseUrl + base.getProduct, { params: params }),// 获取商品列表
-    getDetails: id => global.axios.get(baseUrl + base.getDetails, { params: { id }}), //获取商品详情
+    getDetails: id => global.axios.get(baseUrl + base.getDetails, { params: { id } }), //获取商品详情
+    starProduct: productId => global.axios.post(baseUrl + base.starProduct, { productId }), //收藏商品
+    cancelStarProduct: productId => global.axios.post(baseUrl + base.cancelStarProduct, { productId }), //取消收藏商品
+    getListStared: () => global.axios.get(baseUrl + base.getListStared)
 }
 export default api;
