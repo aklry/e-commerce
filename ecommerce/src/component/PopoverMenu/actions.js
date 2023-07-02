1d23ee81a4d92b1e0a1ae8d43d18d574cfe575f5
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-02 12:15:25 +0800

    商品详情页气泡弹出层菜单的选项

---
+++ ecommerce/src/component/PopoverMenu/actions.js
@@ -0,0 +1,7 @@
+import { AppOutline, AppstoreOutline, UserOutline } from "antd-mobile-icons";
+
+export const actions = [
+    { text: '首页', path: '/', icon: <AppOutline />}, 
+    { text: '分类搜索', path: '/types', icon: <AppstoreOutline />}, 
+    { text: '我的', path: '/my', icon: <UserOutline />}, 
+]
