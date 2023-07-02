782ad1cbafc7d6f86def2370a1f9ecbb72d2fefa
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-02 12:08:07 +0800

    应用body-parser依赖

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = require('./router/index')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use('/api', router)
 
 app.listen(3030, () => {
