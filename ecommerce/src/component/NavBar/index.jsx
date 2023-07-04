3b2fc4a462ea8237566c9fe427d8c58de7e3ebbc
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-04 10:43:37 +0800

    修改顶部导航栏的布局

--- ecommerce/src/component/NavBar/index.jsx
+++ ecommerce/src/component/NavBar/index.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import style from './index.module.css'
 import { NavBar } from 'antd-mobile'
 import { useNavigate } from 'react-router-dom'
+import PopoverMenu from  '../PopoverMenu'
+import { MoreOutline } from 'antd-mobile-icons'
 
 export default function Nav(props) {
     const navigate = useNavigate()
     return (
         <div className={style.nav}>
-            <NavBar onBack={() => navigate(-1)} right={<></>} {...props} />
+            <NavBar onBack={() => navigate(-1)} right={<PopoverMenu>
+                <MoreOutline style={{fontSize:'.4rem'}} />
+            </PopoverMenu>} {...props} />
         </div>
     )
 }
