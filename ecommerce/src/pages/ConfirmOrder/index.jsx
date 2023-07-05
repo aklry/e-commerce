9ad5f231bf500eb52ea615c39943634c4b97c981
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-05 22:55:52 +0800

    完成确认订单页面的布局以及逻辑的实现

--- ecommerce/src/pages/ConfirmOrder/index.jsx
+++ ecommerce/src/pages/ConfirmOrder/index.jsx
@@ -1,20 +1,81 @@
-import { Card, Grid, Space, Stepper } from 'antd-mobile'
-import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { Button, Card, Grid, List, Space, Stepper, Toast } from 'antd-mobile'
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import NavBar from '../../component/NavBar'
+import api from '../../api/address'
+import orderApi from '../../api/orders'
+import { useSelector, useDispatch } from 'react-redux'
+import { setAddress } from '../../store/actions/selectAddress'
 
 export default function ConfirmOrder() {
+    const { address } = useSelector(state => state.address)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
     const location = useLocation()
     const { details } = location.state
     const { selectedAttr = {} } = details
     const [total, setTotal] = useState(details.price * selectedAttr.count)
+    const [count, setCount] = useState(selectedAttr.count)
     const onCountChange = value => {
         setTotal(details.price * value)
+        setCount(value)
+    }
+    useEffect(() => {
+        if (JSON.stringify(address) === '{}') {
+            (async function () {
+                const result = await api.getAddressList()
+                const [records] = result.data.records
+                dispatch(setAddress(records))
+            }())
+        }
+    }, [])
+    const addAddress = () => {
+        navigate('/addressSelect')
+    }
+    //生成订单
+    const onSave = async () => {
+        //判断有没有收货地址
+        if (!address.id) {
+            Toast.show({
+                icon: 'fail',
+                content: '请先添加收货地址'
+            })
+            return
+        }
+        const result = await orderApi.addOrder({
+            product: JSON.stringify({ id: details.id, name: details.name, selectedAttr, mainPic: details.mainPic }),
+            count,
+            addressId: address.id,
+            total
+        })
+        if (result) {
+            Toast.show({
+                icon: 'success',
+                content: '操作成功'
+            })
+            navigate('/orders') //跳转到订单列表页面
+        }
     }
     return (
         <>
             <NavBar>确认订单</NavBar>
             <div className='content'>
+                <List>
+                    <List.Item onClick={addAddress}>
+                        {
+                            JSON.stringify(address) === undefined
+                                ?
+                                <div style={{ textAlign: 'center', color: 'red', padding: '.2rem' }}>
+                                    您还没有收货地址, 点击新增地址
+                                </div>
+                                :
+                                <>
+                                    <h4>{address.name + '  ' + address.tel}</h4>
+                                    <p>{address.labelArea + '  ' + address.address}</p>
+                                </>
+                        }
+                    </List.Item>
+                </List>
                 <Card>
                     <Grid columns={5} gap={20}>
                         <Grid.Item span={2}>
@@ -40,6 +101,9 @@ export default function ConfirmOrder() {
                         <span>免运费</span>
                     </Space>
                 </Card>
+                <div className='footer'>
+                    <Button block color='danger' onClick={onSave}>确定</Button>
+                </div>
             </div>
 
         </>
