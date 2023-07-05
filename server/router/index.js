94295433910fbe2e4cc2183744c7857327dada00
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-05 22:47:16 +0800

    注册编写的接口

--- server/router/index.js
+++ server/router/index.js
@@ -5,9 +5,13 @@ const router = express.Router()
 const types = require('./type')
 const product = require('./product')
 const evaluation = require('./evaluation')
+const address = require('./address')
+const orders = require('./order')
 
 router.use('/types', types)
 router.use('/product', product)
 router.use('/evaluation', evaluation)
+router.use('/address', address)
+router.use('/orders', orders)
 
 module.exports = router
