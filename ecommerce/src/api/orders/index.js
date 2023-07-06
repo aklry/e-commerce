ce6e05aa252ada911f63f016ac876475fc211297
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-06 18:09:25 +0800

    封装用户订单相关操作的方法

--- ecommerce/src/api/orders/index.js
+++ ecommerce/src/api/orders/index.js
@@ -2,12 +2,14 @@ import { baseUrl } from "../base"
 
 const base = {
     getList: '/orders/list',
-    addOrder: '/orders/add'
+    addOrder: '/orders/add',
+    deleteOrder: '/orders/delete',
 }
 
 const api = {
     getOrderList: () => global.axios.get(baseUrl + base.getList),
-    addOrder: params => global.axios.post(baseUrl + base.addOrder, params)
+    addOrder: params => global.axios.post(baseUrl + base.addOrder, params),
+    deleteOrder: params => global.axios.post(baseUrl + base.deleteOrder, params)
 }
 
 export default api
