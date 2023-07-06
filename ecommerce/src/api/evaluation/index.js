f5e72194b87662c09412947aa5720967ffa52172
Aklry <74409739+aklry@users.noreply.github.com>
2023-07-06 18:08:50 +0800

    封装用户评价相关操作(增、删、改、查)的数据

--- ecommerce/src/api/evaluation/index.js
+++ ecommerce/src/api/evaluation/index.js
@@ -3,11 +3,15 @@ import {
 } from "../base";
 
 const base = {
-    getEvaluation: '/evaluation/getEvaluation'
+    getEvaluation: '/evaluation/getEvaluation',
+    addEvaluation: '/evaluation/add',
+    getByOrderId: '/evaluation/getByOrderId'
 }
 
 const api = {
-    getEvaluation: productId => global.axios.get(baseUrl + base.getEvaluation, { params: { productId }}), //获取相关商品的评价
+    getEvaluation: productId => global.axios.get(baseUrl + base.getEvaluation, { params: { productId } }), //获取相关商品的评价
+    addEvaluation: params => global.axios.post(baseUrl + base.addEvaluation, params), //添加商品评价
+    getByOrderId: orderId => global.axios.get(baseUrl + base.getByOrderId, { params: { orderId }}), //根据订单id获取相对应的评价
 }
 
 export default api
